708b043d648a7d0eb6296cf910af2b84911af4c9
semvy <semvy@inbox.ru>
2024-03-08 13:03:59 +0400

    Ver 11

--- src/api.js
+++ src/api.js
@@ -9,13 +9,14 @@ const instance = axios.create({
     headers: {
         "Content-Type": "application/json",
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': '*'
     }
 });
 
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://kalck.ru';
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:8080';
-axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
-axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://kalck.ru';
+// axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
+// axios.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
 axios.defaults.withCredentials = true;
 
 
